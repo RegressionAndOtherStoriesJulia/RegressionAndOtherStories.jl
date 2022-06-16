1349b43251db8ee8b6b70517ff6a25069e1a65c0
Rob J Goedman <goedman@icloud.com>
2022-06-16 16:23:37 -0600

    Rel 0.4.3 - Typo in online docs fixed

--- src/General/model_summary.jl
+++ src/General/model_summary.jl
@@ -13,7 +13,7 @@ $(SIGNATURES)
 ## Keyword arguments
 ```julia
 * `digits = 2` # Number of decimal digits
-* `table-headers` # Default:Symbols or Strings as eltype(pars).
+* `table_headers` # Default:Symbols or Strings as eltype(pars).
 ```
 
 """
