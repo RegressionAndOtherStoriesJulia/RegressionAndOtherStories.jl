6bcc09e3e58940ac152bfd149ba497472d50c24d
Rob J Goedman <goedman@icloud.com>
2022-06-16 16:05:55 -0600

    Rel 0.4.3 - Fix tests for summary

--- test/test_model_summary.jl
+++ test/test_model_summary.jl
@@ -1,4 +1,3 @@
-using StanSample
 using RegressionAndOtherStories
 using Test
 
@@ -10,11 +9,11 @@ df = DataFrame(x = 1:10, y = 1:10)
     @test mod_sum1[:x, :median] ≈ 5.5 atol=0.1
 
     mod_sum2 = model_summary(df, ["x", "y"])
-    @test mod_sum2[:x, :median] ≈ 5.5 atol=0.1
+    @test mod_sum2["x", "median"] ≈ 5.5 atol=0.1
 
     mod_sum3 = model_summary(df, ["x", "y", "z"])
     @test size(mod_sum3) == (2, 4)
-    @test mod_sum3[:x, :median] ≈ 5.5 atol=0.1
+    @test mod_sum3["x", "median"] ≈ 5.5 atol=0.1
 
     mod_sum4 = model_summary(df, ["z"])
     @test mod_sum4 === nothing
