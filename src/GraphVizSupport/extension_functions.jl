8c3ba83ce6947338da350a8ced356440c486e42e
Rob J Goedman <goedman@icloud.com>
2023-07-07 14:30:12 -0600

    Rel 0.13.1

--- src/GraphVizSupport/extension_functions.jl
+++ src/GraphVizSupport/extension_functions.jl
@@ -2,18 +2,13 @@
 
 function create_png_image() end
 function gvplot() end
-function gvplot_g() end
-function gvplot_est_g() end
 function to_gv() end
-function to_gv2() end
 function is_in() end
 function get_mark() end
 
 export
     create_png_image,
     gvplot,
-    gvplot_g,
-    gvplot_est_g,
     is_in,
     get_mark,
     to_gv
