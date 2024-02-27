8a2ffa5339338cf9151dee96989925c39bfb3ae0
Rob J Goedman <goedman@icloud.com>
2024-02-27 13:49:16 -0500

    Rel 0.16.0 - logprob() using nested dataframes

--- src/General/logprob.jl
+++ src/General/logprob.jl
@@ -1,7 +1,24 @@
-function logprob(ndf::DataFrame, x::Matrix, y::Vector, k=k)
+"""
+
+Compute logprobability values for a set of observation values.
+
+$(SIGNATURES)
+
+## Required arguments
+* `ndf::DataFrame`: *Nested* dataframe with parameter set b (b.1, b.2, ...)
+* `mass::Matrix{Floats64}`: Values os size(no of obs, length of b)
+* `obs`: Observation values
+* `sigma::Vector{Float64}`: Sigma values used in logpdf calculation
+* `k::Int`: Length of b vactor
+
+## Return values
+Vector{Float64} of logpdf values
+
+"""
+function logprob(ndf::DataFrame, mass::Matrix, obs::Vector, sigma::Vector{Float64}, k)
     b = Matrix(hcat(ndf.b...)')
-    mu = ndf.a .+ b * x[:, 1:k]'
-    logpdf.(Normal.(mu , ndf.sigma),  y')
+    mu = ndf.a .+ b * mass[:, 1:k]'
+    logpdf.(Normal.(mu , sigma),  obs')
 end
 
 export
